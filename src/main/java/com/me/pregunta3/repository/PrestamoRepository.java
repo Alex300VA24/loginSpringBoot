b09d064e10576e8dc46e06c9fbb24f32356baadd
Alexander Vasquez <josuealcantara251@gmail.com>
2025-02-01 09:46:39 -0500

    Arreglando cosas

--- src/main/java/com/me/pregunta3/repository/PrestamoRepository.java
+++ src/main/java/com/me/pregunta3/repository/PrestamoRepository.java
@@ -8,5 +8,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface PrestamoRepository extends CrudRepository<PrestamoModel, Long>{
 
-    PrestamoModel findById(Long id);
+    //PrestamoModel findById(Long id);
 } 
